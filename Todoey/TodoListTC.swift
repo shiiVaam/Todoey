94319c8ca68470ce368bf7dcfb105fa6d6e020ce
shivam <shivam4vandna@gmail.com>
2019-02-27 14:12:52 +0530

    Added Plist for custom objects rather than using user defaults

--- Todoey/TodoListTC.swift
+++ Todoey/TodoListTC.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class TodoListTC: UITableViewController {
 
-    let defaults = UserDefaults.standard
-  var itemArr = ["find","eggs","be","ready"]
-   
+//    let defaults = UserDefaults.standard
+    var itemArr = [Item]()
+    
+    let dataPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let item = defaults.value(forKey: "ToDoList") as? [String]{
-            itemArr = item
-        }
+
+        print(dataPath!)
+        let item = Item()
+        item.title = "bro"
+        itemArr.append(item)
+        
+        
+        let item1 = Item()
+        item1.title = "fffff"
+        itemArr.append(item1)
+        
+//        if let items = defaults.array(forKey:"ToDoListArray") as? [Item]{
+//            itemArr = items
+//        }
+ 
+        loadItems()
     }
 
     // MARK: - Table view data source
@@ -30,22 +44,23 @@ class TodoListTC: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "todoList", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier:"todoList", for: indexPath)
         
-        cell.textLabel?.text = itemArr[indexPath.row]
+        cell.textLabel?.text = itemArr[indexPath.row].title
     
+        cell.accessoryType = itemArr[indexPath.row].done ?.checkmark:.none
+     
+        
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
-        {
-        tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArr[indexPath.row].done = !itemArr[indexPath.row].done
+        
+       saveItems()
+        
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
@@ -56,16 +71,44 @@ class TodoListTC: UITableViewController {
         let alert = UIAlertController.init(title: "Add new Todey Item", message: "", preferredStyle: .alert)
         alert.addAction(UIAlertAction.init(title: "ADD ITEM", style: .default, handler: { (action) in
             
-            self.itemArr.append(textField.text!)
-            self.tableView.reloadData()
-           
-            self.defaults.set(self.itemArr, forKey: "ToDoList")
-            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArr.append(newItem)
+    
+//            self.defaults.set(self.itemArr, forKey:"ToDoListArray")
+            self.saveItems()
+         
             }))
             alert.addTextField(configurationHandler: { (alertTextField) in
                 alertTextField.placeholder = "Create New Item"
                 textField = alertTextField
         })
         present(alert, animated: true, completion: nil)
+    }
+    func saveItems(){
+        
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(self.itemArr)
+            try data.write(to: self.dataPath!)
+            
+        }catch{
+            
+        }
+        
+        self.tableView.reloadData()
+        
+    }
+    func loadItems(){
+        if let data = try? Data(contentsOf:dataPath!){
+            let decoder = PropertyListDecoder()
+            do{
+                itemArr = try decoder.decode([Item].self, from: data)
+
+            }catch{
+                
+            }
+        }
+        
     }
 }
